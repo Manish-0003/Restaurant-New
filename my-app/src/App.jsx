692158f30013703bdbd3a37ae8d72cec0b8173e1
Manish-0003 <manish.sharma9314@gmail.com>
2025-11-20 21:55:21 +0530

    about page added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 import Home from "./components/Pages/Home/Home";
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar/Navbar";
+import About from "./components/Pages/About/About";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/about" element={<About />} />
         <Route path="/services" element={<div>Services Page</div>} />
         <Route path="/gallery" element={<div>Gallery Page</div>} />
         <Route path="/cart" element={<div>Cart Page</div>} />
--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
+    <>
+    
     <nav className="navbar">
       <div className="nav-logo">Food Fusion</div>
-      <ul className="nav-links">
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeMenu}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link to="/services" onClick={closeMenu}>
+            Services
+          </Link>
         </li>
         <li>
-          <Link to="/gallery">Gallery</Link>
+          <Link to="/gallery" onClick={closeMenu}>
+            Gallery
+          </Link>
         </li>
         <li>
-          <Link to="/cart">Cart</Link>
+          <Link to="/cart" onClick={closeMenu}>
+            Cart
+          </Link>
         </li>
         <li>
-          <Link to="/login" className="login-btn">
+          <Link to="/login" onClick={closeMenu} className="login-btn">
             Login
           </Link>
         </li>
       </ul>
     </nav>
+    </>
   );
 };
 
